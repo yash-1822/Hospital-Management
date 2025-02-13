6f7f711e6fa915c93f7070c0ca7b4730b944226c
yash-1822 <yasandhavarapu@gmail.com>
2025-02-13 17:30:41 +0530

    dgdf

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Make sure this is added to catch all files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
